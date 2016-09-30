b8da7c2f914ccb5be4a644a2b8d6b116e603d175
sekou7 <sekou.yansane320@gmail.com>
2016-09-29 19:34:14 -0700

    Update SpaceFighter.rb

--- Arcade Games/SpaceFighter.rb
+++ Arcade Games/SpaceFighter.rb
@@ -9,49 +9,83 @@ include Gosu # Means you don't have to say "Gosu::"
 
 class SpaceFighter < Gosu::Window
   def initialize
+    game_over = false
+    is_touching = false
     puts "Running SpaceFighter initialize()"
-    super(800, 600, false) # background
-    @background = Background.new(self, 'Arcade Games\\spacebackground2.png')
-    # background
+    super(800, 600, false)
+    @background = Background.new(self, 'Arcade Games\\spacebackground.png')
     @text = Font.new(self, default_font_name, 40) 
-    @spaceship = Sprite.new(self, 'Arcade Games\\Starfighter.bmp')
-    # spaceship code
-    @spaceship.move_to(200,500)
-    # spaceship movement code
-    @enemyship = Sprite.new(self, 'Arcade Games\\enemyship1.png')
-    # enemyship code
-    @enemyship.move_to(550,150)
-    # enemyship movement code
-    @cursor = Gosu::Image.new(self, 'media\\Sprites/arrow.png')
-    # cursor code
+    @ship = Sprite.new(self, 'Arcade Games\\Starfighter.bmp')
+    @ship.move_to(200,500)
     @bullet = Sprite.new(self, 'media\\Sprites/bullet2.png')
-    # bullets code + hides bullets until shown
-    @bullet.move_to(@spaceship.x+@spaceship.width/2, @spaceship.y)
-    #  bullets go where ship moves
-    @bullet = Array.new
-    3.times do
     @bullet.hide
+    @enemyship = Sprite.new(self, 'Arcade Games\\enemyship1.png')
+    @enemyship.move_to(550,150)
+    @cursor = Gosu::Image.new(self, 'media\\Sprites/arrow.png')
+    #@bullet = Sprite.new(self, 'media\\Sprites/bullet2.png')
+    @asteroid1 = Sprite.new(self, 'Arcade Games\\asteroid1.png')
+    @asteroid1.move_to(123,101)
+    @asteroid2 = Sprite.new(self, 'Arcade Games\\asteroid2.png')
+    @asteroid2.move_to(303,146)
+    @asteroid3 = Sprite.new(self, 'Arcade Games\\asteroid3.png')
+    @asteroid3.move_to(510,311)
+    @asteroid4 = Sprite.new(self, 'Arcade Games\\asteroid4.png')
+    @asteroid4.move_to(700,437)
+    @asteroid5 = Sprite.new(self, 'Arcade Games\\asteroid5.png')
+    @asteroid5.move_to(151,391)
+    @is_touching = false
+    @bullet_moving = false
+    @game_over = false
   end
+    
   
   def update
-    @spaceship.adjust_xpos 4 if button_down? KbRight
-    @spaceship.adjust_ypos -4 if button_down? KbUp
-    @spaceship.adjust_ypos 4 if button_down? KbDown
-    @spaceship.adjust_xpos -4 if button_down? KbLeft
+    @ship.adjust_xpos 5 if button_down? KbRight
+    @ship.adjust_ypos -5 if button_down? KbUp
+    @ship.adjust_ypos 5 if button_down? KbDown
+    @ship.adjust_xpos -5 if button_down? KbLeft
     
-    @bullet.adjust_ypos 5 if button_down? KbSpace
+    if @ship.touching?(@asteroid1) or @ship.touching?(@asteroid2) or @ship.touching?(@asteroid3) or @ship.touching?(@asteroid4) or @ship.touching?(@asteroid5)
+      is_touching = true
+      @ship.move_to(30,600)
+    end
     
-  end # for update method
+    @enemyship.adjust_xpos -1
+ 
+    if @bullet.touching?(@enemyship) or @bullet.y < 0
+      @bullet.hide()   
+    end   
+      
+    if button_down? KbSpace and !@bullet.visible?()
+      @bullet.move_to(@ship.x+@ship.width/5, @ship.y-15)
+      @bullet_moving = true
+      @bullet.show()
+    end
+    
   
+    
+    if @bullet_moving
+      @bullet.adjust_ypos -15
+    end
+          
+    
+   end  # for update method
+  
+      
   def draw # Overrides Gosu::Window.draw
     @background.draw
     @text.draw("Welcome to Space Fighter!", 180,0,0)
-    @spaceship.draw
-    @enemyship.draw
-    @cursor.draw self.mouse_x, self.mouse_y, 0
+    @asteroid1.draw
+    @asteroid2.draw
+    @asteroid3.draw
+    @ship.draw
     @bullet.draw
-  end
-  
-  SpaceFighter.new.show # ".show" starts the game, runs once
-  
+    @enemyship.draw
+    @asteroid4.draw
+    @asteroid5.draw
+    @cursor.draw self.mouse_x, self.mouse_y, 0 
+  end # for draw method
+
 end
+    
+SpaceFighter.new.show # ".show" starts the game, runs once
